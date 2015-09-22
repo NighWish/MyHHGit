3f32dc94b05fec5e9cd7c6b1bf733f32517b8775
NightWish <1598004992@qq.com>
2015-09-22 17:19:19 +0800

    createButton

--- TestHHGit/ViewController.m
+++ TestHHGit/ViewController.m
@@ -22,6 +22,11 @@
     self.view.layer.borderColor = [UIColor orangeColor].CGColor;
     self.view.layer.borderWidth = 2.0f;
     self.view.layer.cornerRadius = 10.0f;
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(100, 100, 80, 44);
+    button.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:button];
 }
 
 - (void)didReceiveMemoryWarning {
